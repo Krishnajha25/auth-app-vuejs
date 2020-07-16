6920d56f506ea0d6618abfd596bb9f8c7e007143
Krishna Jha <krishnajha366@gmail.com>
2020-07-16 23:58:53 +0530

    Updated UI of login page: added register form

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,6 @@
 </template>
 
 <script>
-// import Home from './components/Home.vue'
 
 export default {
   name: 'App',
---
+++ src/components/Auth.vue
@@ -0,0 +1,144 @@
+<template>
+    <div class="auth-container">
+        <div class="left">
+
+        </div>
+        <div class="right">
+            <div class="login-form-container" v-if="login">
+                <form method="post" autocomplete="off">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" placeholder="Password">
+                    </div>
+                    <button class="submit-btn" type="submit">
+                        Login
+                    </button>
+                </form>
+                <p class="new-user-cta" @click="registerForm">New user? Register here</p>
+            </div>
+
+            <div class="regiter-form-container" v-if="register">
+                <form method="post" autocomplete="off">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                        <label for="username">Email</label>
+                        <input type="email" name="email" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" placeholder="Password">
+                    </div>
+                    <button class="submit-btn" type="submit">
+                        Register
+                    </button>
+                </form>
+                <p class="new-user-cta" @click="loginForm">Already have account? Login here</p>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            register: false,
+            login: true
+        }
+    },
+    methods: {
+        registerForm(){
+            this.register = true
+            this.login = false
+            // console.log("Login -> ", this.login)
+            // console.log("Register -> ", this.register)
+        },
+        loginForm(){
+            this.register = false
+            this.login = true
+            // console.log("Login -> ", this.login)
+            // console.log("Register -> ", this.register)
+        }
+    },
+    mounted(){
+        console.log('Login page mounted')
+    }
+}
+</script>
+
+<style scoped>
+
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+.auth-container{
+    width: 80%;
+    margin: 0 10%;
+    display: flex;
+    justify-content: space-evenly;
+}
+
+.left{
+    background: url('../assets/login.svg');
+    height: 320px;
+    width: 50%;
+    background-size: contain;
+    background-repeat: no-repeat;
+}
+
+.right{
+    height: 320px;
+    width: 40%;
+    margin: 0 5%;
+}
+
+form{
+    text-align: start;
+}
+
+.form-group {
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+    align-items: flex-start;
+    margin-bottom: 20px;
+}
+
+label{
+    font-size: 14px;
+}
+
+.submit-btn {
+    cursor: pointer;
+    background: #6c63ff;
+    border: none;
+    width: 80px;
+    height: 30px;
+    border-radius: 5px;
+    color: #fff;
+    transition: .2s ease-in-out;
+}
+
+.submit-btn:hover{
+    background: #564fce;
+    box-shadow: 0px 5px 10px #807f7f;
+}
+
+p{
+    cursor: pointer;
+    margin-top: 20px;
+    text-align: start;
+}
+
+</style>
