fff4323e54a883c27f7ff99585939af555af66e3
Krishna Jha <krishnajha366@gmail.com>
2020-07-19 12:20:39 +0530

    Few changes to auth file

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -50,12 +50,13 @@
                 <p class="new-user-cta" @click="loginForm">Already have account? Login here</p>
             </div>
         </div>
-        
+        <div id="snackbar">You can now log in</div>
     </div>
 </template>
 
 <script>
 import firebase from 'firebase'
+// import func from '../../vue-temp/vue-editor-bridge'
 
 export default {
     data(){
@@ -91,6 +92,7 @@ export default {
                         .auth()
                         .signInWithEmailAndPassword(this.loginFormData.email, this.loginFormData.password)
                         .then(() => {
+                            this.showSnackbar()
                             this.$router.replace({name: 'Welcome'})
                         })
                         .catch(err => {
@@ -150,6 +152,14 @@ export default {
         validateEmail(email){
             var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(email)
+        },
+        showSnackbar(){
+            const x = document.getElementById("snackbar")
+            x.className = "show"
+            console.log("Snackbar called")
+            setTimeout(function(){
+                x.className = x.className.replace("show", "")
+            }, 3000)
         }
     },
     mounted(){
@@ -244,5 +254,51 @@ p{
     margin-top: 20px;
     text-align: start;
 }
+
+ /* The snackbar - position it at the bottom and in the middle of the screen */
+#snackbar {
+  visibility: hidden; /* Hidden by default. Visible on click */
+  min-width: 250px; /* Set a default minimum width */
+  margin-left: -125px; /* Divide value of min-width by 2 */
+  background-color: #333; /* Black background color */
+  color: #fff; /* White text color */
+  text-align: center; /* Centered text */
+  border-radius: 2px; /* Rounded borders */
+  padding: 16px; /* Padding */
+  position: fixed; /* Sit on top of the screen */
+  z-index: 1; /* Add a z-index if needed */
+  left: 50%; /* Center the snackbar */
+  bottom: 30px; /* 30px from the bottom */
+}
+
+/* Show the snackbar when clicking on a button (class added with JavaScript) */
+#snackbar.show {
+  visibility: visible; /* Show the snackbar */
+  /* Add animation: Take 0.5 seconds to fade in and out the snackbar.
+  However, delay the fade out process for 2.5 seconds */
+  -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
+  animation: fadein 0.5s, fadeout 0.5s 2.5s;
+}
+
+/* Animations to fade the snackbar in and out */
+@-webkit-keyframes fadein {
+  from {bottom: 0; opacity: 0;}
+  to {bottom: 30px; opacity: 1;}
+}
+
+@keyframes fadein {
+  from {bottom: 0; opacity: 0;}
+  to {bottom: 30px; opacity: 1;}
+}
+
+@-webkit-keyframes fadeout {
+  from {bottom: 30px; opacity: 1;}
+  to {bottom: 0; opacity: 0;}
+}
+
+@keyframes fadeout {
+  from {bottom: 30px; opacity: 1;}
+  to {bottom: 0; opacity: 0;}
+} 
 
 </style>
