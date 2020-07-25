d98d14b8f8d1a090ac390e28593e27c94eb6f2a3
Krishna Jha <krishnajha366@gmail.com>
2020-07-25 23:45:24 +0530

    Change password component updated

--- src/components/Profile/ChangePassword.vue
+++ src/components/Profile/ChangePassword.vue
@@ -3,13 +3,13 @@
         <div class="password-container">
             <form @submit.prevent="changePassword">
                 <div class="form-group">
-                    <v-text-field v-model="changePassword.oldPassword" color="success" :rules="rules" hide-details="auto" @keydown="error=[]" label="Previous password" type="password"></v-text-field>
+                    <v-text-field v-model="changePasswordData.oldPassword" color="success" :rules="rules" hide-details="auto" @keydown="error=[]" label="Previous password" type="password"></v-text-field>
                 </div>
                 <div class="form-group">
-                    <v-text-field v-model="changePassword.newPassword" color="success" :rules="rules" hide-details="auto" @keydown="error=[]" label="New password" type="password"></v-text-field>
+                    <v-text-field v-model="changePasswordData.newPassword" color="success" :rules="rules" hide-details="auto" @keydown="error=[]" label="New password" type="password"></v-text-field>
                 </div>
                 <div class="form-group">
-                    <v-text-field v-model="changePassword.confirmPassword" color="success" :rules="rules" hide-details="auto" @keydown="error=[]" label="Confirm new password" type="password"></v-text-field>
+                    <v-text-field v-model="changePasswordData.confirmPassword" color="success" :rules="rules" hide-details="auto" @keydown="error=[]" label="Confirm new password" type="password"></v-text-field>
                 </div>
                 <v-btn color="primary">Change Password</v-btn>
             </form>
@@ -18,13 +18,40 @@
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
     data(){
         return {
-            changePassword:{
+            changePasswordData:{
                 oldPassword: null,
                 newPassword: null,
                 confirmPassword: null
+            },
+            rules: [
+                value => !!value || 'Required.'
+            ],
+            errors: []
+        }
+    },
+    methods: {
+        changePassword(){
+
+            var user = firebase.auth().currentUser
+
+            if(!this.changePasswordData.oldPassword || !this.changePasswordData.newPassword || !this.changePasswordData.confirmPassword){
+                this.errors.push("All fields are required")
+            }else if(this.changePasswordData.newPassword !== this.changePasswordData.confirmPassword){
+                this.errors.push("Password do not match")
+            }
+            else{
+                user.updatePassword(this.changePasswordData.confirmPassword)
+                .then(() => {
+                    console.log("Password updated")
+                })
+                .catch((err) => {
+                    this.errors.push(err)
+                })
             }
         }
     }
